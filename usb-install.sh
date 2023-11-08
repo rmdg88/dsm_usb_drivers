501bc1aea4cfcc0a9286e367d58266fd9671f745
rmdg88 <66125272+rmdg88@users.noreply.github.com>
2023-11-08 17:07:23 +0000

    Update usb-install.sh

--- usb-install.sh
+++ usb-install.sh
@@ -8,20 +8,20 @@ modprobe cdc-acm
 
 cd /lib/modules/
 
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/ch341.ko
+wget -O ch341.ko https://github.com/rmdg88/dsm_usb_drivers/raw/main/ch341.ko
 insmod /lib/modules/ch341.ko
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/cp210x.ko
+wget -O cp210x.ko https://github.com/rmdg88/dsm_usb_drivers/raw/main/cp210x.ko
 insmod /lib/modules/cp210x.ko
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/pl2303.ko
+wget -O pl2303.ko https://github.com/rmdg88/dsm_usb_drivers/raw/main/pl2303.ko
 insmod /lib/modules/pl2303.ko
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/ti_usb_3410_5052.ko
+wget -O ti_usb_3410_5052.ko https://github.com/rmdg88/dsm_usb_drivers/raw/main/ti_usb_3410_5052.ko
 insmod /lib/modules/ti_usb_3410_5052.ko
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/rndis_host.ko
+wget -O rndis_host.ko https://github.com/rmdg88/dsm_usb_drivers/raw/main/rndis_host.ko
 
 # Configure an autoload script
 
 cd  /usr/local/etc/rc.d/
-wget https://github.com/rmdg88/dsm_usb_drivers/raw/main/start-usb-drivers.sh
+wget -O start-usb-drivers.sh https://github.com/rmdg88/dsm_usb_drivers/raw/main/start-usb-drivers.sh
 chmod +x start-usb-drivers.sh
 
 # Make sure your devices are recongnized
