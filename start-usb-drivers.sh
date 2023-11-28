66b00125806a0b7f979de57eaa93293bd1e1a652
rmdg88 <66125272+rmdg88@users.noreply.github.com>
2023-11-28 19:13:23 +0000

    Update start-usb-drivers.sh

--- start-usb-drivers.sh
+++ start-usb-drivers.sh
@@ -2,8 +2,12 @@
 case $1 in
   start)
     insmod /lib/modules/usbserial.ko > /dev/null 2>&1
+    insmod /lib/modules/ftdi_sio.ko > /dev/null 2>&1
+    insmod /lib/modules/cdc-acm.ko > /dev/null 2>&1
     insmod /lib/modules/cp210x.ko > /dev/null 2>&1
     insmod /lib/modules/ch341.ko > /dev/null 2>&1
+    insmod /lib/modules/pl2303.ko > /dev/null 2>&1
+    insmod /lib/modules/ti_usb_3410_5052.ko > /dev/null 2>&1
     ;;
   stop)
     exit 0
