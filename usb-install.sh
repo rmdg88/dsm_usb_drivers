cbf5071df4a87065e0b0130bfb1c65f90d3c22c5
rmdg88 <66125272+rmdg88@users.noreply.github.com>
2024-02-25 20:58:19 +0000

    Update usb-install.sh

--- usb-install.sh
+++ usb-install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-## (optional) not usb related
-# Check if /dev/net directory exists (needed for qt-wg)
-
-if [ ! -d "/dev/net" ]; then
-    echo "Creating directory /dev/net and tun"
-    mkdir -p /dev/net
-    mknod /dev/net/tun c 10 200
-fi
-
 ## usb related script
 # https://github.com/robertklep/dsm7-usb-serial-drivers/tree/main
 
